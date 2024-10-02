57b7a46587243545768fb60388a2825491f19cc2
dlohinov <dlohinov@cisco.com>
2024-10-02 16:48:27 +0300

    import manualy created recourses to Terraform

--- terraform/create_ec2.tf
+++ terraform/create_ec2.tf
@@ -1,25 +1,25 @@
 resource "aws_instance" "my_ubuntu" {
-    ami = "ami-0e04bcbe83a83792e"
-    instance_type = "t2.micro"
-    
-    tags = {
-        Name = "Ubuntu"
-        Owner = "Demian"
-        Project = "First use Terraform"
+  ami           = "ami-0e04bcbe83a83792e"
+  instance_type = "t2.micro"
 
-    }
-  
-} 
+  tags = {
+    Name    = "Ubuntu"
+    Owner   = "Demian"
+    Project = "First use Terraform"
+
+  }
+
+}
 
 resource "aws_instance" "myAmazonLinux" {
-    ami = "ami-00f07845aed8c0ee7"
-    instance_type = "t2.micro"
+  ami           = "ami-00f07845aed8c0ee7"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "AmazonLinux"
-        Owner = "Demian"
-        Project = "First use Terraform"
+  tags = {
+    Name    = "AmazonLinux"
+    Owner   = "Demian"
+    Project = "First use Terraform"
 
-    }
-  
+  }
+
 } 
