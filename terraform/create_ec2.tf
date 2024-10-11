14d65d5ee2236b4fffc2bc6b133800364242e937
dlohinov <dlohinov@cisco.com>
2024-10-11 21:18:09 +0300

    create SG for Bastion Host and create Bastion Host3

--- terraform/create_ec2.tf
+++ terraform/create_ec2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "Bastion-Host" {
 
   ami = "ami-0162dd7febeafb455"
   instance_type = "t2.micro"
-  subnet_id = element(aws_subnet.public_subnets[1].id)
+  count         = length(var.public_subnet_cidrs)
+  subnet_id = element(aws_subnet.public_subnets[1].id, count.index)
    
   # Security group ID's
   vpc_security_group_ids = [aws_security_group.Bastion-host.id]
